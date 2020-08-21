5fbeb1c3f3bc2f1d06575aad09a25b2b6737aea5
Zhuofan Shen <zs251@scarletmail.rutgers.edu>
2020-08-21 11:14:31 -0400

    Update scatter_plot.py

--- analyzing_results/scatter_plot.py
+++ analyzing_results/scatter_plot.py
@@ -3,20 +3,22 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input_file', type=str, required=True)
-parser.add_argument('-t', '--title', type=str)
-parser.add_argument('-x', '--x_axis', type=int, nargs='*', required=True)
-parser.add_argument('-y', '--y_axis', type=int, nargs='*', required=True)
+parser.add_argument('-x', '--x_line_indexes', type=int, nargs='*', required=True)
+parser.add_argument('-y', '--y_line_indexes', type=int, nargs='*', required=True)
 parser.add_argument('-c', '--colors', type=str, nargs='*')
 parser.add_argument('-m', '--markers', type=str, nargs='*')
+parser.add_argument('-t', '--title', type=str)
+parser.add_argument('-o', '--output_prefix', type=str)
 args = parser.parse_args()
 
 
 with open(args.input_file, 'r') as p_input:
     lines = p_input.readlines()
 
-for i in range(len(args.x_axis)):
-    x_values = eval(lines[args.x_axis[i] - 1]) # In the "lines" list, line number starts at 0 instead of 1.
-    y_values = eval(lines[args.y_axis[i] - 1])
+for i in range(len(args.x_line_indexes)):
+    # In the "lines" list, line number starts at 0 instead of 1.
+    x_values = eval(lines[args.x_line_indexes[i] - 1])
+    y_values = eval(lines[args.y_line_indexes[i] - 1])
     if args.colors:
         color = args.colors[i]
     else:
@@ -36,7 +38,9 @@ axes.set_xlim([-6, 10])
 axes.set_ylim([-6, 10])
 
 fig = plt.gcf()
-fig.set_size_inches(20, 20)
+fig.set_size_inches(5, 5)
 
 plt.show()
-# fig.savefig('test2png.png', dpi=100)
+
+if args.output_prefix:
+    fig.savefig(args.output_prefix + '.png', dpi=100)
