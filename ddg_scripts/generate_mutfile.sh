6467d359d16e45d347ce69a26899afff125c902f
Zhuofan Shen <zs251@scarletmail.rutgers.edu>
2020-08-21 21:58:18 -0400

    Run proteins with duplicated chains

--- ddg_scripts/generate_mutfile.sh
+++ ddg_scripts/generate_mutfile.sh
@@ -5,6 +5,7 @@ do
     -tp) template_pdb="$2";;
     -ml) mutant_list="$2";;
     -cut) cut_region_by_chains="$2";;
+    -dup) duplicated_chains="$2";;
     -ite) iterations="$2";;
     -wl) workload="$2";;
     -part) partition="$2";;
@@ -13,8 +14,9 @@ do
 done
 
 IFS=','
-mutant_list=(${mutant_list[@]})
-cut_region_by_chains=(${cut_region_by_chains[@]})
+mutant_list=(${mutant_list[@]}) # convert mutant_list to array
+cut_region_by_chains=(${cut_region_by_chains[@]}) # convert cut_region_by_chains to array
+duplicated_chains="-dup "${duplicated_chains[@]} # convert duplicated_chains to string
 IFS=' '
 
 if [ -z "${iterations}" ]
@@ -31,7 +33,7 @@ protein=${template_pdb%%"_"*}
 
 if [ ${#mutant_list[@]} -gt 1 ]
 then
-  srun -J match_fasta -p ${partition} -t 20:00 \
+  #srun -J match_fasta -p ${partition} -t 20:00 \
     python3 ../scripts/match_fasta_replicates.py -i ${mutant_list[@]}
 
   for motif_idx in ${!mutant_list[@]}
@@ -43,7 +45,7 @@ then
   chains=$( echo ${cut_region_by_chains[*]} )
   slurmit.py --job ${protein}_0 --partition ${partition} --begin now \
     --command "python3 ../scripts/generate_ddg_mutfile.py -t ${template_pdb} \
-    -m ${fastas} -cut ${chains}"
+    -m ${fastas} -cut ${chains} ${duplicated_chains}"
   sleep 0.1
 
   for motif_idx in ${!mutant_list[@]}
@@ -58,7 +60,7 @@ for motif_idx in ${!mutant_list[@]}
 do
   total_variants=$(expr `grep -o ">" ${mutant_list[$motif_idx]}".fasta.txt" | wc -l` - 1)
   total_jobs=$((${total_variants} * ${iterations} * 2 / ${workload} + 1))
-  srun -J split_${mutant_list[$motif_idx]} -p ${partition} -t 20:00 \
+  #srun -J split_${mutant_list[$motif_idx]} -p ${partition} -t 20:00 \
     python3 ../scripts/split_fasta.py -i ${mutant_list[$motif_idx]}".fasta.txt" \
       -n ${total_jobs} -t ${template_pdb}
 
@@ -72,7 +74,7 @@ do
     slurmit.py --job ${protein}_${job_idx} --partition ${partition} --begin now \
       --command "python3 ../scripts/generate_ddg_mutfile.py -t ${template_pdb} \
       -m ${mutant_list[$motif_idx]}_${job_idx}.fasta.txt \
-      ${cut_region_by_chains[$motif_idx]}"
+      ${cut_region_by_chains[$motif_idx]} ${duplicated_chains}"
     sleep 0.1
   done
 done
