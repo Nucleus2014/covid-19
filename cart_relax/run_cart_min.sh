3ccb3848362ef61cf5f2c86cf0b79795e8b15938
Zhuofan Shen <shenzhuofan01@gmail.com>
2020-10-09 16:58:50 -0400

    Revert "Update run_cart_min.sh"
    This reverts commit 587b81d69d76315dd16d80e1fc2c695a94455f31.

--- cart_relax/run_cart_min.sh
+++ cart_relax/run_cart_min.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --clusters=amarel
-#SBATCH --partition=main
-#SBATCH --job-name=cart
+#SBATCH --partition=p_sdk94_1
+#SBATCH --job-name=orf7
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=2000
+#SBATCH --mem=3000
 #SBATCH --time=3-00:00:00
 #SBATCH --output=slurm.%N.%j.log
 #SBATCH --error=slurm.%N.%j.err
@@ -15,14 +15,15 @@
 #SBATCH --open-mode=append
 
 srun $ROSETTA3/bin/relax.linuxgccrelease \
-  -database $ROSETTA3_DB \
   -s ${1} \
   -use_input_sc \
   -constrain_relax_to_start_coords \
   -ignore_unrecognized_res \
   -fa_max_dis 9.0 \
-  -nstruct 1 \
-  -score:weights ref2015_cart_cst \
-  -relax:min_type lbfgs_armijo_nonmonotone
+  -nstruct 100 \
+  -relax:coord_constrain_sidechains  \
+  -score:weights ref2015_cart \
+  -relax:min_type lbfgs_armijo_nonmonotone \
+  -relax:script ../scripts/cart2.script
 
 exit
